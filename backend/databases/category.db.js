d1444f6e3e805ad27d4c436fed2a35d892c2ee24
Manon Rouquette <mrouquette12110@gmail.com>
2024-10-23 17:44:06 +0200

    Màj .gitignore et category.db

--- backend/databases/category.db.js
+++ backend/databases/category.db.js
@@ -12,58 +12,77 @@ const CategoryDb = {
             return { error: error.message };
         }
     },
-    // // Récupérer toutes les catégories
-    // getAllCategory: async () => {
-    //     try {
-    //         const category = await db.query("SELECT * FROM category");
-    //         return category;
-    //     } catch (error) {
-    //         console.error('Erreur lors de la récupération des catégories :', error);
-    //         throw new Error('Erreur lors de la récupération des catégories');
-    //     }
-    // },
-    // // Récupérer une catégorie par ID
-    // getCategoryById: async (category_id) => {
-    //     try {
-    //         const [category] = await db.query("SELECT * FROM category WHERE category_id = ?", [category_id]);
-    //         return category;
-    //     } catch (error) {
-    //         console.error('Erreur lors de la récupération de la catégorie :', error);
-    //         throw new Error('Erreur lors de la récupération de la catégorie');
-    //     }
-    // },
 
-    // // Mettre à jour une catégorie
-    // updateCategory: async (category_id, updatedCategory) => {
-    //     try {
-    //         if (!updatedCategory || !updatedCategory.category_name) {
-    //             throw new Error('Nom de la catégorie manquant');
-    //         }
+    // Créer une catégorie
+    createCategory: async function (category_name, fk_category_parent) {
+        if (!category_name) {
+            return { error: 'Le nom de la catégorie est requis.' };
+        }
 
-    //         const { category_name } = updatedCategory;
-
-    //         // Mettre à jour la catégorie
-    //         await db.query(
-    //             "UPDATE category SET category_name = ? WHERE category_id = ?",
-    //             [category_name, category_id]
-    //         );
-    //     } catch (error) {
-    //         console.error('Erreur lors de la mise à jour de la catégorie :', error);
-    //         throw new Error('Erreur lors de la mise à jour de la catégorie');
-    //     }
-    // },
-
-    // // Supprimer une catégorie
-    // deleteCategory: async (category_id) => {
-    //     try {
-    //         // Supprimer la catégorie
-    //         await db.query("DELETE FROM category WHERE category_id = ?", [category_id]);
-    //     } catch (error) {
-    //         console.error('Erreur lors de la suppression de la catégorie :', error);
-    //         throw new Error('Erreur lors de la suppression de la catégorie');
-    //     }
-    // }
+        const result = await this.addCategory(category_name, fk_category_parent);
+        if (result.error) {
+            return { error: "Échec de la création de la catégorie" };
+        }
+        return { message: "Catégorie créée avec succès." };
+    }
 };
+
+
+
+
+
+// // Récupérer toutes les catégories
+// getAllCategory: async () => {
+//     try {
+//         const category = await db.query("SELECT * FROM category");
+//         return category;
+//     } catch (error) {
+//         console.error('Erreur lors de la récupération des catégories :', error);
+//         throw new Error('Erreur lors de la récupération des catégories');
+//     }
+// },
+// // Récupérer une catégorie par ID
+// getCategoryById: async (category_id) => {
+//     try {
+//         const [category] = await db.query("SELECT * FROM category WHERE category_id = ?", [category_id]);
+//         return category;
+//     } catch (error) {
+//         console.error('Erreur lors de la récupération de la catégorie :', error);
+//         throw new Error('Erreur lors de la récupération de la catégorie');
+//     }
+// },
+
+// // Mettre à jour une catégorie
+// updateCategory: async (category_id, updatedCategory) => {
+//     try {
+//         if (!updatedCategory || !updatedCategory.category_name) {
+//             throw new Error('Nom de la catégorie manquant');
+//         }
+
+//         const { category_name } = updatedCategory;
+
+//         // Mettre à jour la catégorie
+//         await db.query(
+//             "UPDATE category SET category_name = ? WHERE category_id = ?",
+//             [category_name, category_id]
+//         );
+//     } catch (error) {
+//         console.error('Erreur lors de la mise à jour de la catégorie :', error);
+//         throw new Error('Erreur lors de la mise à jour de la catégorie');
+//     }
+// },
+
+// // Supprimer une catégorie
+// deleteCategory: async (category_id) => {
+//     try {
+//         // Supprimer la catégorie
+//         await db.query("DELETE FROM category WHERE category_id = ?", [category_id]);
+//     } catch (error) {
+//         console.error('Erreur lors de la suppression de la catégorie :', error);
+//         throw new Error('Erreur lors de la suppression de la catégorie');
+//     }
+// }
+;
 
 export default CategoryDb;
 
