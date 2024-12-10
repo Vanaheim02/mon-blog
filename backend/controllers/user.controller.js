c1e052b42a0ae195b4645331d22878ab1e3cd799
Svein SAMSON <ssamson@cabinet-bedin-immobilier.com>
2024-12-10 18:07:29 +0100

    Mise en commentaire code inutile ATM

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -44,6 +44,8 @@ const UserController = {
                 return res.status(500).json({ message: "Erreur lors de la création de l'utilisateur." });
             }
 
+            // TODO: Créer le profil (requête à profile.db.js)
+
             return res.status(201).json({ message: "Utilisateur créé avec succès !" });
         } catch (error) {
 
@@ -52,104 +54,104 @@ const UserController = {
         }
     },
 
-    // Fonction pour mettre à jour l'état de l'utilisateur
-    updateUserState: async (req, res) => {
+    // // Fonction pour mettre à jour l'état de l'utilisateur
+    // updateUserState: async (req, res) => {
 
-        try {
-            const { user_state, user_date_in, user_date_out} = req.body;
-            const user_id = req.params.id;
+    //     try {
+    //         const { user_state, user_date_in, user_date_out} = req.body;
+    //         const user_id = req.params.id;
 
-            if (!user_id || !user_state || !user_date_in) {
-                return res.status(400).json({ error: "Tous les champs sont obligatoires" });
-            }
+    //         if (!user_id || !user_state || !user_date_in) {
+    //             return res.status(400).json({ error: "Tous les champs sont obligatoires" });
+    //         }
 
-            const result = await UserDb.updateUserState(user_id, user_state, user_date_in, user_date_out);
+    //         const result = await UserDb.updateUserState(user_id, user_state, user_date_in, user_date_out);
 
-            if (result && result.affectedRows > 0) {
-                return res.status(200).json({ message: "État de l'utilisateur mis à jour avec succès" });
-            } else {
+    //         if (result && result.affectedRows > 0) {
+    //             return res.status(200).json({ message: "État de l'utilisateur mis à jour avec succès" });
+    //         } else {
 
-                return res.status(404).json({ error: "Utilisateur non trouvé ou erreur lors de la mise à jour" });
-            }
-        } catch (error) {
+    //             return res.status(404).json({ error: "Utilisateur non trouvé ou erreur lors de la mise à jour" });
+    //         }
+    //     } catch (error) {
 
-            console.error("Erreur  lors de la mise à jour de l'état de l'utilisateur");
-            return res.status(500).json({ error: "Erreur lors de la mise à jour" });
-        }
-    },
-
-
-     // Ajouter des permissions à un utilisateur
-    createPermission: async (req, res) => {
-        try {
-            const { permission_label, permission_slug } = req.body;
-
-        if (!permission_label || !permission_slug) {
-            return res.status(400).json({ error: "Le label et l'identifiant de la permission sont obligatoires" });
-        }
-
-        const result = await UserDb.createPermission(permission_label, permission_slug);
-
-        if (result && result.affectedRows > 0) {
-            return res.status(201).json({ message: "Permission créée avec succès" });
-        } else {
-            return res.status(500).json({ error: "Erreur lors de la création des permissions" });
-        }
-
-    } catch (error) {
-        console.error("Erreur lors de la création des permissions");
-        return res.status(500).json({ error: "Erreur lors de la création des permissions" });
-    }
-},
-
-    // Liée les permissions au profils
-
-    permissionProfil: async (req, res) => {
-        try {
-            const { fk_permission_id, fk_profil_id } = req.body;
-
-            if (!fk_permission_id || !fk_profil_id) {
-                return res.status(400).json({ error: "Les identifiants de permission et de profil sont requis" });
-            }
-
-            const result = await UserDb.permissionProfil(fk_permission_id, fk_profil_id);
-
-            if (result && result.affectedRows > 0) {
-                return res.status(201).json({ message: "Liaison du profil et de la permission réussie" });
-            } else {
-                return res.status(500).json({ error: "Erreur lors de la liaison du profil et de la permission" });
-            }
-        } catch (error) {
-            console.error("Erreur lors de la liaison du profil et de la permission");
-            return res.status(500).json({ error: "Une erreur interne est survenue" });
-        }
-    },
+    //         console.error("Erreur  lors de la mise à jour de l'état de l'utilisateur");
+    //         return res.status(500).json({ error: "Erreur lors de la mise à jour" });
+    //     }
+    // },
 
 
+    //  // Ajouter des permissions à un utilisateur
+    // createPermission: async (req, res) => {
+    //     try {
+    //         const { permission_label, permission_slug } = req.body;
 
-     // Profil utilisateur :
+    //     if (!permission_label || !permission_slug) {
+    //         return res.status(400).json({ error: "Le label et l'identifiant de la permission sont obligatoires" });
+    //     }
 
-     getUserProfile: async (req, res) => {
-        try {
-            const user_id = req.params.id;
+    //     const result = await UserDb.createPermission(permission_label, permission_slug);
 
-            if (!user_id) {
-                console.log("Aucun ID utilisateur n'est indiqué")
-                return res.status(400).json({ error: "L'ID utilisateur est requis" });
-            }
+    //     if (result && result.affectedRows > 0) {
+    //         return res.status(201).json({ message: "Permission créée avec succès" });
+    //     } else {
+    //         return res.status(500).json({ error: "Erreur lors de la création des permissions" });
+    //     }
 
-            const profile = await UserDb.getProfileByUserId(user_id);
+    // } catch (error) {
+    //     console.error("Erreur lors de la création des permissions");
+    //     return res.status(500).json({ error: "Erreur lors de la création des permissions" });
+    // }
+    // },
 
-            if (profile.length > 0) {
-                res.status(200).json({message: "Profil utilisateur récupéré avec succès"})
-            } else {
-                res.status(404).json({ error: "Profil non trouvé" });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "Erreur lors de la récupération du profil utilisateur" });
-        }
-    }
+    // // Liée les permissions au profils
+
+    // permissionProfil: async (req, res) => {
+    //     try {
+    //         const { fk_permission_id, fk_profil_id } = req.body;
+
+    //         if (!fk_permission_id || !fk_profil_id) {
+    //             return res.status(400).json({ error: "Les identifiants de permission et de profil sont requis" });
+    //         }
+
+    //         const result = await UserDb.permissionProfil(fk_permission_id, fk_profil_id);
+
+    //         if (result && result.affectedRows > 0) {
+    //             return res.status(201).json({ message: "Liaison du profil et de la permission réussie" });
+    //         } else {
+    //             return res.status(500).json({ error: "Erreur lors de la liaison du profil et de la permission" });
+    //         }
+    //     } catch (error) {
+    //         console.error("Erreur lors de la liaison du profil et de la permission");
+    //         return res.status(500).json({ error: "Une erreur interne est survenue" });
+    //     }
+    // },
+
+
+
+    //  // Profil utilisateur :
+
+    //  getUserProfile: async (req, res) => {
+    //     try {
+    //         const user_id = req.params.id;
+
+    //         if (!user_id) {
+    //             console.log("Aucun ID utilisateur n'est indiqué")
+    //             return res.status(400).json({ error: "L'ID utilisateur est requis" });
+    //         }
+
+    //         const profile = await UserDb.getProfileByUserId(user_id);
+
+    //         if (profile.length > 0) {
+    //             res.status(200).json({message: "Profil utilisateur récupéré avec succès"})
+    //         } else {
+    //             res.status(404).json({ error: "Profil non trouvé" });
+    //         }
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).json({ error: "Erreur lors de la récupération du profil utilisateur" });
+    //     }
+    // }
 
     // updateUserProfile: async (req, res) => {
     //     try {
@@ -205,44 +207,6 @@ const UserController = {
     //if (userResponse.error) {
     //return res.status(500).json({ message: userResponse.error });
     //}
-    // // Connexion
-    // login: async (req, res) => {
-    //     try {
-    //         const { mail, password } = req.body;
-
-    //         // Vérifier la validité de l'email
-    //         if (!mail || !isEmail(mail)) {
-    //             return res.status(403).json({ message: `Email invalide` });
-    //         }
-
-    //         // Vérifier si le mot de passe est renseigné
-    //         if (!stringIsFilled(password)) {
-    //             return res.status(403).json({ message: `Mot de passe invalide` });
-    //         }
-
-    //         const response = await UserDb.signIn(mail);
-    //         if (!response)
-    //             return res.status(401).json({ message: `Échec de l'authentification` });
-
-    //         if (!await compareHash(password, response.user_password))
-    //             return res.status(401).json({ error: "Email ou mot de passe incorrect." });
-
-    //         let id_user = response.id_user;
-    //         const token = jwtSign(id_user);
-
-    //         // Stockage des données de session dans l'objet req.session
-    //         req.session.isLoggedIn = true;
-    //         req.session.mail = response.user_mail;
-    //         req.session.isAdmin = response.rank === 'admin';
-
-    //         return res
-    //             .status(200)
-    //             .json({ message: "Connexion réussie !", user: { id_user, mail: response.user_mail }, token });
-    //     } catch (error) {
-    //         console.error("Erreur lors de la connexion de l'utilisateur");
-    //         res.status(500).json({ error: "Erreur lors de la connexion de l'utilisateur." });
-    //     }
-    // },
     // // Fonction pour que l'utilisateur puisse changer de mot de passe
     // updatePassword: async (req, res) => {
     //     try {
