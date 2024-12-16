8c2abb131a2ac1f0f6dca10bfbd2c9e90a263b3e
Svein SAMSON <ssamson@cabinet-bedin-immobilier.com>
2024-12-16 16:12:08 +0100

    - Article, routes : Commentaire code inutile ATM

--- backend/routes/article.routes.js
+++ backend/routes/article.routes.js
@@ -6,20 +6,20 @@ const jsonParser = bodyParser.json();
 const articleRouter = express.Router();
 
 const initArticleRoutes = (app) => {
-    // Pour récupérer toutes les articles
-    articleRouter.get('/', jsonParser, ArticleController.listArticle);
+    // // Pour récupérer toutes les articles
+    // articleRouter.get('/', jsonParser, ArticleController.listArticle);
 
     // Route pour ajouter une nouvelle article
     articleRouter.post('/add', jsonParser, ArticleController.addArticle);
 
-    // Route pour mettre à jour un article
-    articleRouter.put('/update/:id', jsonParser, ArticleController.updateArticle);
+    // // Route pour mettre à jour un article
+    // articleRouter.put('/update/:id', jsonParser, ArticleController.updateArticle);
 
-    // Route pour supprimer un article
-    articleRouter.delete('/delete/:id', jsonParser, ArticleController.deleteArticle)
+    // // Route pour supprimer un article
+    // articleRouter.delete('/delete/:id', jsonParser, ArticleController.deleteArticle)
 
-    // Récupérer un article par son Id
-    articleRouter.get('/read-:id', jsonParser, ArticleController.getArticleById);
+    // // Récupérer un article par son Id
+    // articleRouter.get('/read-:id', jsonParser, ArticleController.getArticleById);
 
     // // Pagination des article
     // articleRouter.get('/page', jsonParser, ArticleController.listPaginationArticle)
